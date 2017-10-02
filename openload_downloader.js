99c2600ac051c767685083c61e2b629b6d7b8029
Vincent Marnier <vincent.marnier@gmail.com>
2017-10-02 13:39:14 +0200

    first commit

---
+++ openload_downloader.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         openloaddownloader
+// @namespace    com.vincentmarnier.openloaddownloader
+// @version      0.1
+// @description  Add download link to openload streaming link
+// @author       VincentMarnier
+// @include      http://openload.co/embed/*
+// @include      https://openload.co/embed/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const downloadUrl = document.URL.toString().replace("embed", "f");
+    const header = `<div id="OpenLoadDownloaderHeader" style="background: black; color: white; display: flex; flex-direction: row">
+                      <span style="flex:1"><b>OpenloadDownloader:</b> <a href="` + downloadUrl + `" target="_top" style="color:grey">Download link</a></span>
+                      <a id="OpenLoadDownloaderHeaderClose" href="#" style="color:grey">[x]</a>
+                    </div>`;
+    document.body.innerHTML = header + document.body.innerHTML;
+    document.getElementById('OpenLoadDownloaderHeaderClose').addEventListener("click", () => { document.getElementById('OpenLoadDownloaderHeader').style.display = 'none'; });
+})();
